b106d31e6f80ebbc3c9e6939cd2d8af5aec24990
peter.varga@drei.com <peter.varga@drei.com>
2020-12-23 11:14:04 +0100

    Initial project add

---
+++ src/main/java/guru/springframework/katapetclilnic/KataPetClilnicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.katapetclilnic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataPetClilnicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataPetClilnicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/katapetclilnic/KataPetClilnicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.katapetclilnic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataPetClilnicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
